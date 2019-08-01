d53bc53fc63d2173a0344088800399002cf64883
mlvzk <mlvzk@protonmail.com>
2019-08-01 18:59:21 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod lexer;
 mod parser;
+mod util;
 
 use std::fmt;
 use std::io;
@@ -28,13 +29,12 @@ impl fmt::Display for ShellVar<'_> {
             })
             .collect::<String>();
 
-        write!(f, r#"{}="{}""#, escaped_name, self.value)
+        write!(f, r#"{}="{}"{}"#, escaped_name, self.value, "\n")
     }
 }
 
 impl parser::Value {
-    fn traverse<F>(self, namespace: String, f: &mut F)
-        where F: FnMut(ShellVar) -> (),
+    fn traverse(self, namespace: &mut String, f: &mut impl FnMut(ShellVar) -> ())
     {
         use parser::Value;
         match self {
@@ -44,13 +44,21 @@ impl parser::Value {
             Value::False => f(ShellVar::new(&namespace, "false")),
             Value::Null => f(ShellVar::new(&namespace, "null")),
             Value::Array(arr) => {
+                namespace.push('_');
+                let n_len = namespace.len();
                 for (i, v) in arr.into_iter().enumerate() {
-                    v.traverse(format!("{}_{}", namespace, i), f);
+                    namespace.push_str(util::usize_to_str(i));
+                    v.traverse(namespace, f);
+                    namespace.truncate(n_len);
                 }
             }
             Value::Object(obj) => {
+                namespace.push('_');
+                let n_len = namespace.len();
                 for (key, value) in obj {
-                    value.traverse(format!("{}_{}", namespace, key), f);
+                    namespace.push_str(&key);
+                    value.traverse(namespace, f);
+                    namespace.truncate(n_len);
                 }
             }
         }
@@ -58,15 +66,19 @@ impl parser::Value {
 }
 
 fn main() -> Result<(), io::Error> {
-    let mut stdin = io::stdin();
+//    let mut stdin = io::stdin();
+    let mut stdin = std::fs::File::open("/nix/store/9g6g97m31n5ns47fpcri70397mx5cz3y-python2.7-botocore-1.12.96/lib/python2.7/site-packages/botocore/data/ec2/2016-11-15/service-2.json")?;
     let mut s = String::new();
     stdin.read_to_string(&mut s)?;
     let l = lexer::Lexer::new(&s);
     let p = parser::Parser::new(l);
 
-    let mut stdout = io::stdout();
+    let mut stdout = io::BufWriter::new(io::stdout());
+//    let mut stdout = io::sink();
     for v in p {
-        v.traverse("root".to_string(), &mut |var| {
+        let mut namespace = String::with_capacity(60);
+        namespace.push_str("root");
+        v.traverse(&mut namespace, &mut |var: ShellVar| {
             stdout.write_all(var.to_string().as_bytes()).unwrap();
         });
     }
